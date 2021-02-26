1fe8b19935f954a5825a8ef70d30df587db7ff9b
rqsell <68514875+rqsell@users.noreply.github.com>
2021-02-26 15:26:19 -0500

    get distance

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import logo from "./logo.svg";
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import GeoLocation from "./GeoLocation";
-import getDistance from "./getDistance";
 function App() {
   const location = GeoLocation();
 
+  const GetDistance = () => {
+    let lat1 = 48.8566;
+    let lon1 = 2.3522;
+    let lat2 = location.coordinates.latitude;
+    let lon2 = location.coordinates.longitude;
+    const R = 6371e3; // metres
+    const φ1 = (lat1 * Math.PI) / 180; // φ, λ in radians
+    const φ2 = (lat2 * Math.PI) / 180;
+    const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+    const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
+    const a =
+      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+      Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const d = R * c; // in metres
+    return <div> The distance in meters from paris france is {d}</div>;
+  };
   return (
     <div className="App">
       <h1>Location App</h1>
       {location.loaded
         ? JSON.stringify(location.coordinates)
         : "Location data is not yet available"}
-      {getDistance()}
+      {GetDistance()}
     </div>
   );
 }
