98d9bfeb96675605a08a417c94ad4b572606dfe2
rqsell <68514875+rqsell@users.noreply.github.com>
2021-02-26 13:43:12 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import GeoLocation from "./GeoLocation";
 function App() {
+  const location = GeoLocation();
   return (
     <div className="App">
       <h1>Location App</h1>
+      {location.loaded
+        ? JSON.stringify(location)
+        : "Location data is not yet available"}
     </div>
   );
 }
